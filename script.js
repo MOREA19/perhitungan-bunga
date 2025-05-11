d25092754c8281d42d1138bd5313169cb565bdec
More <lexyvanila@gmail.com>
2025-05-12 02:28:42 +0700

    halo dek

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.section, .choice-section');
+const options = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    section.classList.add('fade-init');
+    observer.observe(section);
+});
+
